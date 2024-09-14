7895d8af3a0c00657fc507f0fa9ecd9d4995e365
Hydro <jermaine.strand@yahoo.com>
2024-09-14 14:30:50 -0400

    some changes

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
+// Serve static files from the 'dist' folder
+app.use(express.static('dist'));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+// Middleware to parse JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// TODO: Implement middleware to connect the routes
+// Middleware to connect the routes
 app.use(routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
